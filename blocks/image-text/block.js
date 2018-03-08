c2967a2d6df45b90a3f296086c1fccb52b5b5a31
Thomas W <kontakt@themecoder.de>
2018-03-08 13:35:03 +0100

    created own Component for Image & Text Edit Block function

---
+++ blocks/image-text/block.js
@@ -0,0 +1,239 @@
+/**
+ * Internal block libraries
+ */
+const { Component } = wp.element;
+const { __ } = wp.i18n;
+const {
+    AlignmentToolbar,
+    BlockControls,
+    ImagePlaceholder,
+    InspectorControls,
+    MediaUpload,
+    registerBlockType,
+    RichText,
+} = wp.blocks;
+
+const {
+    Button,
+    DropZone,
+    FormFileUpload,
+    IconButton,
+    Placeholder,
+    SelectControl,
+} = wp.components;
+
+const {
+    mediaUpload,
+} = wp.utils;
+
+
+const columnSizes = [
+	{ value: 'block-column-25', label: __( '25%' ) },
+	{ value: 'block-column-33', label: __( '33%' ) },
+	{ value: 'block-column-40', label: __( '40%' ) },
+	{ value: 'block-column-50', label: __( '50%' ) },
+    { value: 'block-column-66', label: __( '66%' ) },
+];
+
+class gtImageTextBlock extends Component {
+    constructor() {
+        super( ...arguments );
+
+        this.onSelectImage = this.onSelectImage.bind( this );
+        this.onRemoveImage = this.onRemoveImage.bind( this );
+        this.onChangeTitle = this.onChangeTitle.bind( this );
+        this.onChangeText = this.onChangeText.bind( this );
+        this.onChangeAlignment = this.onChangeAlignment.bind( this );
+        this.onSetActiveEditable = this.onSetActiveEditable.bind( this );
+        this.onChangeColumnSize = this.onChangeColumnSize.bind( this );
+    }
+
+    onSelectImage( img ) {
+        this.props.setAttributes( {
+            imgID: img.id,
+            imgURL: img.url,
+            imgAlt: img.alt,
+        } );
+    }
+
+    onRemoveImage() {
+        this.props.setAttributes( {
+            imgID: null,
+            imgURL: null,
+            imgAlt: null,
+        } );
+    }
+
+    setImage( [ image ] ) {
+        onSelectImage( image )
+    };
+
+    uploadFromFiles( event ) {
+        mediaUpload( event.target.files, this.setImage );
+    }
+
+    onFilesDrop( files ) {
+        mediaUpload( files, this.setImage );
+    }
+
+    onHTMLDrop( HTML ) {
+        this.setImage( map(
+            rawHandler( { HTML, mode: 'BLOCKS' } )
+                .filter( ( { name } ) => name === 'core/image' ),
+            'attributes'
+        ) );
+    }
+
+    onChangeTitle( newTitle ) {
+        this.props.setAttributes( { title: newTitle } )
+    }
+
+    onChangeText( newText ) {
+        this.props.setAttributes( { text: newText } )
+    }
+
+    onChangeAlignment( newAlignment ) {
+        this.props.setAttributes( { alignment: newAlignment } );
+    }
+
+    onSetActiveEditable( newEditable ) {
+        this.props.setAttributes( { editable: newEditable  } );
+    }
+
+    onChangeColumnSize( newColumnSize ) {
+        this.props.setAttributes( { columnSize: newColumnSize  } );
+    }
+
+    render() {
+        const { attributes, setAttributes, isSelected, className } = this.props;
+
+        return (
+            <div className={ `${className} ${attributes.columnSize}` }>
+
+                <div className="block-image">
+                    {
+                        isSelected && (
+                            <InspectorControls key="inspector">
+
+                                <SelectControl
+                                    label={ __( 'Image Size' ) }
+                                    value={ attributes.columnSize }
+                                    onChange={ this.onChangeColumnSize }
+                                    options={ columnSizes }
+                                />
+
+                            </InspectorControls>
+                        )
+                    }
+
+                    { ! attributes.imgID ? (
+
+                        <Placeholder
+                            className="block-image-placeholder"
+                            instructions={ __( 'Drag image here or add from media library' ) }
+                            icon="format-image"
+                            label={ __( 'Image' ) } >
+
+                            <DropZone
+                                onFilesDrop={ this.onFilesDrop }
+                                onHTMLDrop={ this.onHTMLDrop }
+                            />
+
+                            <FormFileUpload
+                                isLarge
+                                className="wp-block-image__upload-button"
+                                onChange={ this.uploadFromFiles }
+                                accept="image/*"
+                            >
+                                { __( 'Upload test' ) }
+                            </FormFileUpload>
+
+                            <MediaUpload
+                                onSelect={ this.onSelectImage }
+                                type="image"
+                                render={ ( { open } ) => (
+                                    <Button isLarge onClick={ open }>
+                                        { __( 'Add from Media Library' ) }
+                                    </Button>
+                                ) }
+                            />
+                        </Placeholder>
+
+                    ) : (
+
+                        <div class="image-wrapper">
+
+                            <img
+                                src={ attributes.imgURL }
+                                alt={ attributes.imgAlt }
+                            />
+
+                            { isSelected ? (
+                                <div class="image-buttons">
+                                    <MediaUpload
+                                        onSelect={ this.onSelectImage }
+                                        type="image"
+                                        value={ attributes.imgID }
+                                        render={ ( { open } ) => (
+                                            <IconButton
+                                                className="edit-image"
+                                                label={ __( 'Edit Image' ) }
+                                                icon="edit"
+                                                onClick={ open }
+                                            />
+                                        ) }
+                                    />
+                                    <IconButton
+                                        className="remove-image"
+                                        label={ __( 'Remove Image' ) }
+                                        icon="trash"
+                                        onClick={ this.onRemoveImage }
+                                    />
+                                </div>
+                            ) : null }
+
+                        </div>
+                    ) }
+
+                </div>
+
+                <div className="block-content">
+                    {
+                        isSelected && (
+                            <BlockControls key="controls">
+                                <AlignmentToolbar
+                                    value={ attributes.alignment }
+                                    onChange={ this.onChangeAlignment }
+                                />
+                            </BlockControls>
+                        )
+                    }
+                    <RichText
+                        tagName="h2"
+                        placeholder={ __( 'Enter a title' ) }
+                        value={ attributes.title }
+                        className="block-title"
+                        style={ { textAlign: attributes.alignment } }
+                        onChange={ this.onChangeTitle }
+                        isSelected={ isSelected && attributes.editable === 'title' }
+                        onFocus={ () => this.onSetActiveEditable( 'title' ) }
+                    />
+
+                    <RichText
+                        tagName="div"
+                        multiline="p"
+                        placeholder={ __( 'Enter your text here.' ) }
+                        value={ attributes.text }
+                        className="block-text"
+                        style={ { textAlign: attributes.alignment } }
+                        onChange={ this.onChangeText }
+                        isSelected={ isSelected && attributes.editable === 'text' }
+                        onFocus={ () => this.onSetActiveEditable( 'text' ) }
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+export default gtImageTextBlock;
--- blocks/image-text/index.js
+++ blocks/image-text/index.js
@@ -1,45 +1,15 @@
 /**
  * Block dependencies
  */
-import classnames from 'classnames';
 import './style.scss';
 import './editor.scss';
+import { default as gtImageTextBlock } from './block';
 
 /**
  * Internal block libraries
  */
 const { __ } = wp.i18n;
-const {
-    AlignmentToolbar,
-    BlockControls,
-    ImagePlaceholder,
-    InspectorControls,
-    MediaUpload,
-    registerBlockType,
-    RichText,
-} = wp.blocks;
-
-const {
-    Button,
-    DropZone,
-    FormFileUpload,
-    IconButton,
-    Placeholder,
-    SelectControl,
-} = wp.components;
-
-const {
-    mediaUpload,
-} = wp.utils;
-
-
-const columnSizes = [
-	{ value: 'block-column-25', label: __( '25%' ) },
-	{ value: 'block-column-33', label: __( '33%' ) },
-	{ value: 'block-column-40', label: __( '40%' ) },
-	{ value: 'block-column-50', label: __( '50%' ) },
-    { value: 'block-column-66', label: __( '66%' ) },
-];
+const { registerBlockType } = wp.blocks;
 
 /**
  * Register block
@@ -93,171 +63,7 @@ registerBlockType(
                 default: 'block-column-50'
             }
         },
-        edit: props => {
-            const onSelectImage = img => {
-                props.setAttributes( {
-                    imgID: img.id,
-                    imgURL: img.url,
-                    imgAlt: img.alt,
-                } );
-            };
-            const onRemoveImage = () => {
-                props.setAttributes( {
-                    imgID: null,
-                    imgURL: null,
-                    imgAlt: null,
-                } );
-            };
-            const setImage = ( [ image ] ) => onSelectImage( image );
-            const uploadFromFiles = ( event ) => mediaUpload( event.target.files, setImage );
-            const onFilesDrop = ( files ) => mediaUpload( files, setImage );
-            const onHTMLDrop = ( HTML ) => setImage( map(
-                rawHandler( { HTML, mode: 'BLOCKS' } )
-                    .filter( ( { name } ) => name === 'core/image' ),
-                'attributes'
-            ) );
-            const onChangeTitle = newTitle => {
-                props.setAttributes( { title: newTitle } )
-            };
-            const onChangeText = newText => {
-                props.setAttributes( { text: newText } )
-            };
-            const onChangeAlignment = newAlignment => {
-                props.setAttributes( { alignment: newAlignment } );
-            };
-            const onSetActiveEditable = ( newEditable ) => () => {
-                props.setAttributes( { editable: newEditable  } );
-            };
-            const onChangeColumnSize = newColumnSize => {
-                props.setAttributes( { columnSize: newColumnSize  } );
-            };
-            return (
-                <div className={ `${props.className} ${props.attributes.columnSize}` }>
-
-                    <div className="block-image">
-                        {
-                            props.isSelected && (
-                                <InspectorControls key="inspector">
-
-                                    <SelectControl
-                                        label={ __( 'Image Size' ) }
-                                        value={ props.attributes.columnSize }
-                                        onChange={ onChangeColumnSize }
-                                        options={ columnSizes }
-                                    />
-
-                                </InspectorControls>
-                            )
-                        }
-
-                        { ! props.attributes.imgID ? (
-
-                            <Placeholder
-                                className="block-image-placeholder"
-                                instructions={ __( 'Drag image here or add from media library' ) }
-                                icon="format-image"
-                                label={ __( 'Image' ) } >
-
-                                <DropZone
-                                    onFilesDrop={ onFilesDrop }
-                                    onHTMLDrop={ onHTMLDrop }
-                                />
-
-                                <FormFileUpload
-                                    isLarge
-                                    className="wp-block-image__upload-button"
-                                    onChange={ uploadFromFiles }
-                                    accept="image/*"
-                                >
-                                    { __( 'Upload test' ) }
-                                </FormFileUpload>
-
-                                <MediaUpload
-                                    onSelect={ onSelectImage }
-                                    type="image"
-                                    render={ ( { open } ) => (
-                                        <Button isLarge onClick={ open }>
-                                            { __( 'Add from Media Library' ) }
-                                        </Button>
-                                    ) }
-                                />
-                            </Placeholder>
-
-                        ) : (
-
-                            <div class="image-wrapper">
-
-                                <img
-                                    src={ props.attributes.imgURL }
-                                    alt={ props.attributes.imgAlt }
-                                />
-
-                                { props.isSelected ? (
-                                    <div class="image-buttons">
-                                        <MediaUpload
-                                            onSelect={ onSelectImage }
-                                            type="image"
-                                            value={ props.attributes.imgID }
-                                            render={ ( { open } ) => (
-                                                <IconButton
-                                                    className="edit-image"
-                                                    label={ __( 'Edit Image' ) }
-                                                    icon="edit"
-                                                    onClick={ open }
-                                                />
-                                            ) }
-                                        />
-                                        <IconButton
-                                            className="remove-image"
-                                            label={ __( 'Remove Image' ) }
-                                            icon="trash"
-                                            onClick={ onRemoveImage }
-                                        />
-                                    </div>
-                                ) : null }
-
-                            </div>
-                        ) }
-
-                    </div>
-
-                    <div className="block-content">
-                        {
-                            props.isSelected && (
-                                <BlockControls key="controls">
-                                    <AlignmentToolbar
-                                        value={ props.attributes.alignment }
-                                        onChange={ onChangeAlignment }
-                                    />
-                                </BlockControls>
-                            )
-                        }
-                        <RichText
-                            tagName="h2"
-                            placeholder={ __( 'Enter a title' ) }
-                            value={ props.attributes.title }
-                            className="block-title"
-                            style={ { textAlign: props.attributes.alignment } }
-                            onChange={ onChangeTitle }
-                            isSelected={ props.isSelected && props.attributes.editable === 'title' }
-                            onFocus={ onSetActiveEditable( 'title' ) }
-                        />
-
-                        <RichText
-                            tagName="div"
-                            multiline="p"
-                            placeholder={ __( 'Enter your text here.' ) }
-                            value={ props.attributes.text }
-                            className="block-text"
-                            style={ { textAlign: props.attributes.alignment } }
-                            onChange={ onChangeText }
-                            isSelected={ props.isSelected && props.attributes.editable === 'text' }
-                            onFocus={ onSetActiveEditable( 'text' ) }
-                        />
-                    </div>
-                </div>
-            );
-        },
+        edit: gtImageTextBlock,
         save: props => {
             return (
                 <div className={props.attributes.columnSize}>
