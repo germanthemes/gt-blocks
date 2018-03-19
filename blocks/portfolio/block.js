606b3725f08273afb8b0887cc150a85798a291c9
Thomas W <kontakt@themecoder.de>
2018-03-19 19:37:15 +0100

    added button to add new portfolio items

--- blocks/portfolio/block.js
+++ blocks/portfolio/block.js
@@ -1,19 +1,54 @@
 /**
  * Internal block libraries
  */
-const { Component } = wp.element;
-const { __ } = wp.i18n;
-const {
-    RichText,
-} = wp.blocks;
+ const { Component, compose } = wp.element;
+ const { __, sprintf } = wp.i18n;
+ const {
+     AlignmentToolbar,
+     BlockControls,
+     ColorPalette,
+     ImagePlaceholder,
+     InspectorControls,
+     MediaUpload,
+     registerBlockType,
+     RichText,
+ } = wp.blocks;
+
+ const {
+     Button,
+     DropZone,
+     FormFileUpload,
+     IconButton,
+     PanelBody,
+     PanelColor,
+     Placeholder,
+     RangeControl,
+     SelectControl,
+     TextControl,
+     ToggleControl,
+     Toolbar,
+     Tooltip,
+     withAPIData,
+ } = wp.components;
+
+ const {
+     mediaUpload,
+ } = wp.utils;
 
 class gtPortfolioBlock extends Component {
     constructor() {
         super( ...arguments );
 
+        this.addPortfolioItem = this.addPortfolioItem.bind( this );
         this.onChangeTitle = this.onChangeTitle.bind( this );
         this.onChangeText = this.onChangeText.bind( this );
         this.onSetActiveEditable = this.onSetActiveEditable.bind( this );
+    }
+
+    addPortfolioItem() {
+        let newItems = [...this.props.attributes.items];
+        newItems.push( { 'title': '', 'text': '' } );
+        this.props.setAttributes( { items: newItems } );
     }
 
     onChangeTitle( newTitle, index ) {
@@ -71,6 +106,15 @@ class gtPortfolioBlock extends Component {
                     }
 
                 </div>
+
+                { isSelected && (
+                    <Button
+                        isLarge
+                        onClick={ this.addPortfolioItem }
+                    >
+                        { __( 'Add portfolio item' ) }
+                    </Button>
+                ) }
             </div>
         );
     }
