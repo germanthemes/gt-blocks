33f37784b21bc77f031ba186696a422e965c374a
Thomas Weichselbaumer <kontakt@themecoder.de>
2018-07-25 11:15:50 +0200

    added columns setting to GT Services

--- blocks/services/index.js
+++ blocks/services/index.js
@@ -1,6 +1,7 @@
 /**
  * External dependencies
  */
+import classnames from 'classnames';
 import { times } from 'lodash';
 import memoize from 'memize';
 
@@ -20,9 +21,18 @@ import './editor.scss';
  */
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
+const { Fragment } = wp.element;
+
 const {
+    BlockAlignmentToolbar,
+    InspectorControls,
     InnerBlocks,
 } = wp.editor;
+
+const {
+    PanelBody,
+    RangeControl,
+} = wp.components;
 
 /**
  * InnerBlock Settings
@@ -37,7 +47,7 @@ const ALLOWED_BLOCKS = [ 'german-themes-blocks/service-item' ];
  * @return {Object[]} Columns layout configuration.
  */
 const getServicesTemplate = memoize( ( services ) => {
-	return times( services, () => [ 'german-themes-blocks/service-item', { text: 'Enter your service' } ] );
+    return times( services, () => [ 'german-themes-blocks/service-item', { text: 'Enter your service' } ] );
 } );
 
 /**
@@ -72,28 +82,65 @@ registerBlockType(
                 type: 'number',
                 default: 3,
             },
+            blockAlignment: {
+                type: 'string',
+                default: 'center',
+            },
         },
 
         edit( { attributes, setAttributes, isSelected, className } ) {
 
+            const classNames= classnames( className, {
+                [ `gt-columns-${ attributes.columns }` ]: attributes.columns,
+            } );
+
             return (
-                <div className={ className }>
+                <Fragment>
 
-                    <InnerBlocks
-                        template={ getServicesTemplate( attributes.services ) }
-                        templateLock='all'
-                        allowedBlocks={ ALLOWED_BLOCKS }
-                    />
+                    <InspectorControls key="inspector">
 
-                </div>
+                        <PanelBody title={ __( 'Layout Settings' ) } initialOpen={ false } className="gt-panel-layout-settings gt-panel">
+
+                            <RangeControl
+                                label={ __( 'Columns' ) }
+                                value={ attributes.columns }
+                                onChange={ ( nextColumns ) => setAttributes( { columns: nextColumns } ) }
+                                min={ 2 }
+                                max={ 6 }
+                            />
+
+                        </PanelBody>
+
+                    </InspectorControls>
+
+                    <div className={ classNames }>
+                        <div className="gt-grid-container">
+
+                            <InnerBlocks
+                                template={ getServicesTemplate( attributes.services ) }
+                                templateLock='all'
+                                allowedBlocks={ ALLOWED_BLOCKS }
+                            />
+
+                        </div>
+                    </div>
+
+                </Fragment>
             );
         },
 
         save( { attributes } ) {
 
+            const classNames = classnames( {
+                [ `gt-columns-${ attributes.columns }` ]: attributes.columns,
+                [ `align${ attributes.blockAlignment }` ]: ( 'wide' === attributes.blockAlignment || 'full' === attributes.blockAlignment ),
+            } );
+
             return (
-                <div>
-                    <InnerBlocks.Content />
+                <div className={ classNames ? classNames : undefined }>
+                    <div className="gt-grid-container">
+                        <InnerBlocks.Content />
+                    </div>
                 </div>
             );
         },
