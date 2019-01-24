e3e9513051636f5f8513ed4168a0b1b0b4d8490a
Thomas Weichselbaumer <kontakt@themecoder.de>
2019-01-24 14:57:47 +0100

    remove unregisterBlockType code

--- src/plugins/deactivate-blocks/index.js
+++ src/plugins/deactivate-blocks/index.js
@@ -6,24 +6,7 @@ const { assign } = window.lodash;
 /**
  * WordPress dependencies
  */
-//const { unregisterBlockType } = wp.blocks;
-//const { select } = wp.data;
 const { addFilter } = wp.hooks;
-
-/* Unregistering blocks breaks Editor if blocks were already added
-wp.domReady( function() {
-	// Get Plugin Options.
-	const options = select( 'gt-blocks-store' ).getPluginOptions();
-
-	// Unregister deactivated blocks.
-	Object.keys( options ).forEach( ( block ) => {
-		// Check if block is deactivated.
-		if ( false === options[ block ] ) {
-			unregisterBlockType( block );
-		}
-	} );
-} );
-*/
 
 function removeBlockFromInserter( settings ) {
 	function isBlockDisabled( block ) {
