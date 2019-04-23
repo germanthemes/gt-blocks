9c27a3e66ebf7b4fb08962c86c01d708950ef36c
Thomas Weichselbaumer <kontakt@themecoder.de>
2019-04-23 10:51:42 +0200

    updated image card block to use new component

--- src/blocks/image-card/index.js
+++ src/blocks/image-card/index.js
@@ -8,7 +8,6 @@ import classnames from 'classnames';
  */
 const { __ } = wp.i18n;
 const { registerBlockType } = wp.blocks;
-const { Fragment } = wp.element;
 
 /**
  * Internal dependencies
@@ -16,6 +15,8 @@ const { Fragment } = wp.element;
 import './style.scss';
 import './editor.scss';
 import edit from './edit';
+import imageBlockAttributes from '../../components/image-block/attributes';
+import ImageBlock from '../../components/image-block';
 
 /**
  * Register block
@@ -32,75 +33,34 @@ registerBlockType(
 		icon: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 448H48c-26.51 0-48-21.49-48-48V112c0-26.51 21.49-48 48-48h416c26.51 0 48 21.49 48 48v288c0 26.51-21.49 48-48 48zM112 120c-30.928 0-56 25.072-56 56s25.072 56 56 56 56-25.072 56-56-25.072-56-56-56zM64 384h384V272l-87.515-87.515c-4.686-4.686-12.284-4.686-16.971 0L208 320l-55.515-55.515c-4.686-4.686-12.284-4.686-16.971 0L64 336v48z" /></svg>,
 
 		attributes: {
-			id: {
-				type: 'number',
-			},
-			url: {
+			imagePosition: {
 				type: 'string',
-				source: 'attribute',
-				selector: 'img',
-				attribute: 'src',
+				default: 'left',
 			},
-			alt: {
+			verticalAlignment: {
 				type: 'string',
-				source: 'attribute',
-				selector: 'img',
-				attribute: 'alt',
-				default: '',
+				default: 'top',
 			},
-			size: {
-				type: 'string',
-				default: 'full',
-			},
-			maxWidth: {
-				type: 'string',
-				default: '100',
-			},
-			href: {
-				type: 'string',
-				source: 'attribute',
-				selector: 'figure > a',
-				attribute: 'href',
-			},
-			linkDestination: {
-				type: 'string',
-				default: 'none',
-			},
+			...imageBlockAttributes,
 		},
 
 		edit,
 
-		save( { attributes } ) {
+		save( props ) {
 			const {
-				id,
-				url,
-				alt,
-				maxWidth,
-				href,
-			} = attributes;
+				imagePosition,
+				verticalAlignment,
+			} = props.attributes;
 
 			const blockClasses = classnames( {
-				[ `gt-max-width-${ maxWidth }` ]: '100' !== maxWidth,
+				[ `gt-image-position-${ imagePosition }` ]: 'left' !== imagePosition,
+				[ `gt-vertical-align-${ verticalAlignment }` ]: 'top' !== verticalAlignment,
 			} );
 
-			const image = (
-				<img
-					src={ url }
-					alt={ alt }
-					className={ id ? `wp-image-${ id }` : null }
-				/>
-			);
-
-			const figure = (
-				<Fragment>
-					{ href ? <a href={ href }>{ image }</a> : image }
-				</Fragment>
-			);
-
 			return (
-				<figure className={ blockClasses ? blockClasses : undefined }>
-					{ figure }
-				</figure>
+				<div className={ blockClasses }>
+					<ImageBlock { ...props } />
+				</div>
 			);
 		},
 	},
