6af2091d33153537f4d141a70fb99150a88a09b5
Thomas Weichselbaumer <kontakt@themecoder.de>
2019-05-20 10:52:13 +0200

    removed Card Style of GT Features and GT Portfolio block

--- src/blocks/features/edit.js
+++ src/blocks/features/edit.js
@@ -1,27 +1,8 @@
 /**
- * External dependencies
- */
-const { find } = lodash;
-
-/**
  * WordPress dependencies
  */
 const { __ } = wp.i18n;
-const { compose } = wp.compose;
-const { withDispatch } = wp.data;
-const TokenList = wp.tokenList;
-
-const {
-	Component,
-	Fragment,
-} = wp.element;
-
-const { InspectorControls } = wp.editor;
-
-const {
-	PanelBody,
-	SelectControl,
-} = wp.components;
+const { Component } = wp.element;
 
 /**
  * Internal dependencies
@@ -43,88 +24,14 @@ const TEMPLATE = [
  * Block Edit Component
  */
 class FeaturesEdit extends Component {
-	getActiveStyle( styles, className ) {
-		for ( const style of new TokenList( className ).values() ) {
-			if ( style.indexOf( 'is-style-' ) === -1 ) {
-				continue;
-			}
-
-			const potentialStyleName = style.substring( 9 );
-			const activeStyle = find( styles, { value: potentialStyleName } );
-			if ( activeStyle ) {
-				return activeStyle;
-			}
-		}
-
-		return find( styles, 'isDefault' );
-	}
-
-	replaceActiveStyle( className, activeStyle, newStyle ) {
-		const list = new TokenList( className );
-
-		if ( activeStyle ) {
-			list.remove( 'is-style-' + activeStyle.value );
-		}
-
-		list.add( 'is-style-' + newStyle );
-
-		return list.value;
-	}
-
 	render() {
-		const {
-			attributes,
-			onChangeClassName,
-		} = this.props;
-
-		const blockStyles = [
-			{ value: 'default', label: __( 'Default', 'gt-blocks' ), isDefault: true },
-			{ value: 'card', label: __( 'Card', 'gt-blocks' ) },
-		];
-
-		const activeStyle = this.getActiveStyle( blockStyles, attributes.className );
-
-		const updateClassName = ( newStyle ) => {
-			const updatedClassName = this.replaceActiveStyle( attributes.className, activeStyle, newStyle );
-			onChangeClassName( updatedClassName );
-		};
-
 		return (
-			<Fragment>
-
-				<InspectorControls>
-
-					<PanelBody title={ __( 'Styles', 'gt-blocks' ) } initialOpen={ false } className="gt-panel-styles gt-panel">
-
-						<SelectControl
-							label={ __( 'Styles', 'gt-blocks' ) }
-							value={ activeStyle.value }
-							onChange={ ( newStyle ) => updateClassName( newStyle ) }
-							options={ blockStyles }
-						/>
-
-					</PanelBody>
-
-				</InspectorControls>
-
-				<GridEdit
-					template={ TEMPLATE }
-					{ ...this.props }
-				/>
-
-			</Fragment>
+			<GridEdit
+				template={ TEMPLATE }
+				{ ...this.props }
+			/>
 		);
 	}
 }
 
-export default compose( [
-	withDispatch( ( dispatch, { clientId } ) => {
-		return {
-			onChangeClassName( newClassName ) {
-				dispatch( 'core/editor' ).updateBlockAttributes( clientId, {
-					className: newClassName,
-				} );
-			},
-		};
-	} ),
-] )( FeaturesEdit );
+export default FeaturesEdit;
